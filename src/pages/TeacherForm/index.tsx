007e1f594c3321ce0d53a4de592e0d2cad29da0b
Gerson Salvador <gerson.salvador@outlook.com>
2020-08-05 21:30:12 -0300

    TeacherForm and TeacherList pages

--- src/pages/TeacherForm/index.tsx
+++ src/pages/TeacherForm/index.tsx
@@ -2,13 +2,103 @@ import React from 'react'
 // import { Link } from 'react-router-dom';
 
 import PageHeader from '../../components/PageHeader'
+import Input from '../../components/Input';
+
+import warningIcon from '../../assets/images/icons/warning.svg'
 
 import './styles.css'
+import Select from '../../components/Select';
+import Textarea from '../../components/Textarea';
 
 function TeacherForm(){
+
+  const fieldsets = [
+    {
+      title: "Your Info",
+      inputs: [
+        {label: "Full name", name:"name"},
+        {label: "Avatar", name:"avatar"},
+        {label: "Whatsapp", name:"Whatsapp"}
+      ],
+      textareas:[
+        {label: "Bio", name: "bio"}
+      ]
+    },
+    {
+      title: "Abput the class",
+      selects: [
+        {label: "Subject", name:"subject", options: [
+          {value: "Artes", label: "Artes"},
+          {value: "Biologia", label: "Biologia"},
+          {value: "Ciências", label: "Ciências"},
+          {value: "Educação Física", label: "Educação Física"},
+          {value: "Física", label: "Física"},
+          {value: "Geografia", label: "Geografia"},
+          {value: "História", label: "História"},
+          {value: "Matemática", label: "Matemática"},
+          {value: "Português", label: "Português"},
+          {value: "Química", label: "Química"},
+        ]},
+      ],
+      inputs: [
+        {label: "Cost per hour", name:"cost"},
+      ]
+    }
+  ]
+
   return (
     <div id="page-teacher-form" className="container">
-      <PageHeader title="Que incrível que voce quer dar aulas" />
+      <PageHeader 
+        title="It's amazing taht you want to give classes"
+        description="Fill up the form and you're ready to go."
+      />
+
+      <main>
+
+          {fieldsets.map(({title, selects, inputs, textareas}) => (
+            <fieldset>
+              <legend>{title}</legend>
+              {selects?.map(item => <Select {...item}/>)}
+              {inputs?.map(item => <Input {...item}/>)}
+              {textareas?.map(item => <Textarea {...item}/>)}
+            </fieldset>
+          ))}
+        <fieldset>
+          <legend>
+            Schedules Available
+            <button>+ New Schedule</button>
+          </legend>
+          <div className="schedule-items">
+            {[
+              {label: "Subject", name:"subject", options: [
+                {value: "Artes", label: "Artes"},
+                {value: "Biologia", label: "Biologia"},
+                {value: "Ciências", label: "Ciências"},
+                {value: "Educação Física", label: "Educação Física"},
+                {value: "Física", label: "Física"},
+                {value: "Geografia", label: "Geografia"},
+                {value: "História", label: "História"},
+                {value: "Matemática", label: "Matemática"},
+                {value: "Português", label: "Português"},
+                {value: "Química", label: "Química"},
+              ]},
+            ].map(item => <Select {...item}/>)}
+
+            <Input label="From" name="from"/>
+            <Input label="To" name="to"/>
+          </div>
+        </fieldset>
+        <footer>
+          <p>
+            <img src={warningIcon} alt="Warning"/>
+            Atention! <br/>
+            Fill up all fields
+          </p>
+          <button>
+            Send Info
+          </button>
+        </footer>
+      </main>
     </div>
   )
 }
--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -4,24 +4,48 @@ import React from 'react'
 import './styles.css'
 import PageHeader from '../../components/PageHeader'
 import TeacherItem from '../../components/TeacheItem'
+import Input from '../../components/Input'
+import Select from '../../components/Select'
 
 function TeacherList(){
+          
+  const 
+    inputs = [
+      {type: "time",name: "time", label:"Time"}
+    ]
+    
+    const selects = [
+      {label: "Subject", name:"subject", options: [
+        {value: "Artes", label: "Artes"},
+        {value: "Biologia", label: "Biologia"},
+        {value: "Ciências", label: "Ciências"},
+        {value: "Educação Física", label: "Educação Física"},
+        {value: "Física", label: "Física"},
+        {value: "Geografia", label: "Geografia"},
+        {value: "História", label: "História"},
+        {value: "Matemática", label: "Matemática"},
+        {value: "Português", label: "Português"},
+        {value: "Química", label: "Química"},
+      ]},
+      {label: "Week Day", name:"week_day", options: [
+        {value: "0", label: "Domingo"},
+        {value: "1", label: "Segunda-Feira"},
+        {value: "2", label: "Terça-Feira"},
+        {value: "3", label: "Quart-Feira"},
+        {value: "4", label: "Quinta-Feira"},
+        {value: "5", label: "Sexta-Feira"},
+        {value: "6", label: "Sábado"},
+      ]},
+    ]
+
   return (
     <div id="page-teacher-list" className="container">
       <PageHeader title="Estes são so proffys disponíveis.">
         <form  id="search-teachers" action="">
-          <div className="input-block">
-            <label htmlFor="subject">Subject</label>
-            <input type="text" id="subject"/>
-          </div>
-          <div className="input-block">
-            <label htmlFor="week_day">Week Day</label>
-            <input type="text" id="week_day"/>
-          </div>
-          <div className="input-block">
-            <label htmlFor="time">Time</label>
-            <input type="text" id="time"/>
-          </div>
+
+          {selects?.map(item => <Select {...item}/>)}
+          {inputs.map(item => <Input {...item}/>)}
+
         </form>
       </PageHeader>
       <main>
